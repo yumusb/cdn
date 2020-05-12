22b20a00e1751f0bf549220c62033dbd4d01e68a
榆木 <43062104+yumusb@users.noreply.github.com>
2020-05-12 23:02:38 +0800

    Update and rename Quantumult-X.js to Quantumult-X1.js

--- js/Quantumult-X1.js
+++ js/Quantumult-X1.js
@@ -5,9 +5,9 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = obj['country'];
-var subtitle = obj['city'] + ' ' + obj['isp'];
+var title = obj['country_code'];
+var subtitle =  obj['isp'];
 var ip = obj['ip'];
-var description = obj['country'] + '\n' + obj['city'] + '\n' + obj['isp'];
+var description = obj['country'] + '\n' + obj['isp'];
 
 $done({title, subtitle, ip, description});
